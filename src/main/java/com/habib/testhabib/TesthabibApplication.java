963b05942371bfc53f514622510ebfbd360cebed
Habib <habib@justlogin.com>
2026-01-13 19:30:06 +0700

    [DEV] Init boilerplate

---
+++ src/main/java/com/habib/testhabib/TesthabibApplication.java
@@ -0,0 +1,13 @@
+package com.habib.testhabib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthabibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesthabibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habib/testhabib/TesthabibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habib.testhabib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthabibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
